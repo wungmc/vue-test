3a1900f9089668a73c3b97b65f2223b1fdb13156
wungmc <wungcc@gmail.com>
2019-03-19 18:06:22 +0800

    ant-design-vue 引入

--- src/component/secondcomponent.vue
+++ src/component/secondcomponent.vue
@@ -1,39 +1,48 @@
 <template>
-    <div id="secondcomponent">
-        <h2>2 page</h2>
-        <a href="#">writted by {{ author }} at {{ date }}</a>
+  <div id="secondcomponent">
+    <h2>2 page</h2>
+    <a href="#">writted by {{ author }} at {{ date }}</a>
 
-        <h2>豆瓣电影Top10</h2>
-        <ul>
-            <li v-for="article in articles" :key="article.id">
-                {{ article.title }}
-            </li>
-        </ul>
-    </div>
+    <a-card title="豆瓣电影Top10" :bordered="false" style="width: 300px">
+      <!-- <p>Card content</p>
+      <p>Card content</p>
+      <p>Card content</p> -->
+      <p v-for="article in articles" :key="article.id">
+        {{ article.title }}
+      </p>
+    </a-card>
+    <h2>豆瓣电影Top10</h2>
+    <ul>
+      <li v-for="article in articles" :key="article.id">
+        {{ article.title }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
-    data () {
-        return {
-            author: 'wungmc',
-            date: '2019.3.18',
-            articles: []
-        }
+  export default {
+    data() {
+      return {
+        author: 'wungmc',
+        date: '2019.3.18',
+        articles: []
+      }
     },
-    mounted: function() {
-        this.$http.jsonp('https://api.douban.com/v2/movie/top250?count=10', {}, {
-            headers: {
+    mounted: function () {
+      this.$http.jsonp('https://api.douban.com/v2/movie/top250?count=10', {}, {
+        headers: {
 
-            },
-            emulateJSON: true
-        }).then(function(response) {
-            // 处理正确的回调函数
-            this.articles = response.data.subjects
-        }, function(response) {
-            // 处理错误的回调函数
-            console.log(response)
-        })
+        },
+        emulateJSON: true
+      }).then(function (response) {
+        // 处理正确的回调函数
+        this.articles = response.data.subjects
+      }, function (response) {
+        // 处理错误的回调函数
+        console.log(response)
+      })
     }
-}
+  }
+
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 
+Vue.config.productionTip = false
+
 Vue.use(VueRouter)
 Vue.use(VueResource)
+Vue.use(Antd)
 
 // 也可以使用之前定义的 firstcomponent
 const first = { template: '<div><h2>我是第 1 个子页面</h2></div>' }
