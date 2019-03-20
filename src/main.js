fbee4c0d72da37e6ada2adfb7f22badad431da9c
wungmc <wungcc@gmail.com>
2019-03-20 11:00:07 +0800

    formate code

--- src/main.js
+++ src/main.js
@@ -14,27 +14,28 @@ Vue.use(VueResource)
 Vue.use(Antd)
 
 // 也可以使用之前定义的 firstcomponent
-const first = { template: '<div><h2>我是第 1 个子页面</h2></div>' }
+const first = {
+    template: '<div><h2>我是第 1 个子页面</h2></div>'
+}
 import secondcomponent from './component/secondcomponent.vue'
 
 const router = new VueRouter({
-  mode: 'history',
-  // 跟目录
-  base: '__dirname',
-  routes: [
-    {
-      path: '/first',
-      component: first
-    },
-    {
-      path: '/second',
-      component: secondcomponent
-    }
-  ]
+    mode: 'history',
+    // 跟目录
+    base: '__dirname',
+    routes: [{
+            path: '/first',
+            component: first
+        },
+        {
+            path: '/second',
+            component: secondcomponent
+        }
+    ]
 })
 
 const app = new Vue({
-  el: '#app',
-  router: router,
-  render: h => h(App)
+    el: '#app',
+    router: router,
+    render: h => h(App)
 })
